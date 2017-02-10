03f5727f482abf1c4706c0dbe7983d07ae029196
Abdulrahman Sahmoud <ahsahmoud@ucdavis.edu>
2017-02-09 21:59:47 -0800

    fixed app.js to configure body parser before using routes

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(require('./routes/admin'));
-
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(require('./routes/admin'));
+
 
 var mongodb = require('./modules/db');
 mongodb.connect("mongodb://stockmanagement:1234567890aA@ds139909.mlab.com:39909/chefoncall", function () {
